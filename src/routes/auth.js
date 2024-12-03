50b11517bf56cad779487c8ee33c9745a1c2b59f
Jérémy PION <Jeremy.pionsfr@gmail.com>
2024-12-03 12:22:41 +0100

    fix: fix architecture

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,29 +1,28 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const users = require('../models/user')
+const users = require("../models/user");
 
 // Placeholder routes for authentication
-router.get('/login', (req, res) => res.render('login'));
-router.post('/login', (req, res) => {
-    users.authenticate(req.body.username, req.body.password, (user) => {
-        if (user.connected) {
-            res.redirect('/?userId=' + user.id)
-        }
-    })
-})
-router.get('/user/register', (req, res) => {
-    let username = req.query.username;
-    var password = req.query.password;
-    let email = req.query.email;
-    users.create({ username, password, email }, (err, user) => {
-        if (user) {
-            res.redirect('/login');
-        } else {
-            res.redirect('/')
-        }
-    })
-})
-router.get('/register', (req, res) => res.render('register'));
+router.get("/login", (req, res) => res.render("login"));
+router.post("/login", (req, res) => {
+  users.authenticate(req.body.username, req.body.password, (user) => {
+    if (user.connected) {
+      res.redirect("/?userId=" + user.id);
+    }
+  });
+});
+router.get("/user/register", (req, res) => {
+  let username = req.query.username;
+  var password = req.query.password;
+  let email = req.query.email;
+  users.create({ username, password, email }, (err, user) => {
+    if (user) {
+      res.redirect("/login");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+router.get("/register", (req, res) => res.render("register"));
 
 module.exports = router;
